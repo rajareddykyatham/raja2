bf806a712bb0e015b1c9aea6c98feab86b4d565d
rajareddykyatham <rajareddykyatham5@gmail.com>
2020-01-28 11:36:41 +0530

    my first commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+	public static void main(String[] args) {
+             int a,b,c;
+	     a=45;
+	     b=75;
+	     c=105;
+	     c=a+b+c;
+	     System.out.println("the sum of three values:"+c);
+	}
+}
